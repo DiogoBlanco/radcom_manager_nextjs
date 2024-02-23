a4943b5f1ab4025a0b9c9a4190d1c4c84ba9f865
DiogoBlanco <diogoblanco@outlook.com.br>
2024-02-23 20:46:50 -0300

    css

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
 import styles from "./page.module.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "Diogo Blanco",
 };
 
 export default function HomePage() {
-  return <h1 className={styles.main_title}>Contratos</h1>;
+  return (
+    <>
+      <div className={styles.navbar}>
+        <Navbar></Navbar>
+      </div>
+      <section className={styles.main_content}>
+        <h1 className={styles.main_title}>Contratos</h1>
+      </section>
+    </>
+  );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,44 @@
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "../public/logo_radcom.png";
+import styles from "../styles/navbar.module.css";
 
 export default function Navbar() {
-  return <nav></nav>;
+  return (
+    <nav className={styles.main_nav}>
+      <ul className={styles.nav_ul}>
+        <li>
+          <Link href="#">
+            <Image
+              className={styles.logo}
+              src={Logo}
+              alt="radcom-logo"
+              width={80}
+              heigth={80}
+            ></Image>
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.nav_li} href="#">
+            Clientes
+          </Link>
+        </li>
+        <li className={styles.nav_li}>
+          <Link className={styles.nav_li} href="#">
+            Contratos
+          </Link>
+        </li>
+        <li className={styles.nav_li}>
+          <Link className={styles.nav_li} href="#">
+            Atendimentos
+          </Link>
+        </li>
+        <li className={styles.nav_li}>
+          <Link className={styles.nav_li} href="#">
+            Anotações
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
